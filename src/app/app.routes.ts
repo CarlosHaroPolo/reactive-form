80c3f0cbeb34897c9fdd37f10391ee61c28b609e
Carlos Haro Polo <carlosisaachp18@gmail.com>
2025-03-09 22:53:18 -0500

    9.2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+  path:'reactive',
+  loadChildren:()=>import('./reactive/reactive.routes').then(r=>r.reactiveRoutes),
+},
+{
+path:'auth',
+loadChildren:()=>import('./auth/auth.routes').then(r=>r.authRoutes),
+},
+{
+path:'country',
+loadChildren:()=>import('./country/country.routes')
+},
+{
+  path:'**',
+  redirectTo:'reactive'
+}];
